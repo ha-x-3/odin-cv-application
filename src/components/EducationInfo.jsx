5d42659a3125d8b34f83aca328ae7e6e3aa502e2
Brittney Epperson <beppers87@hotmail.com>
2024-08-16 14:04:48 -0500

    Added education component

--- src/components/EducationInfo.jsx
+++ src/components/EducationInfo.jsx
@@ -1,7 +1,43 @@
 import React from 'react'
 
-export default function EducationInfo() {
-  return (
-    <div className='educationInfo main'></div>
-  )
+export default function EducationInfo({ educationInfo, setEducationInfo  }) {
+    const handleChange = (e) => {
+      const { name, value } = e.target;
+      setEducationInfo((prev) => ({...prev, [name]: value }));
+    };
+
+    return (
+        <div className='educationInfo main'>
+            <label>
+                Degree:
+                <input
+                    type='text'
+                    name='degree'
+                    value={educationInfo.degree}
+                    onChange={handleChange}
+                    placeholder='Bachelor of Science'
+                />
+            </label>
+            <label>
+                Institution:
+                <input
+                    type='text'
+                    name='institution'
+                    value={educationInfo.institution}
+                    onChange={handleChange}
+                    placeholder='University of Example'
+                />
+            </label>
+            <label>
+                Graduation Year:
+                <input
+                    type='number'
+                    name='graduationYear'
+                    value={educationInfo.graduationYear}
+                    onChange={handleChange}
+                    placeholder='2020'
+                />
+            </label>
+        </div>
+    )
 }
--- src/components/Preview.jsx
+++ src/components/Preview.jsx
@@ -8,7 +8,10 @@ export default function Preview({ generalInfo, educationInfo, workInfo }) {
 			<p>Email: {generalInfo.email}</p>
 			<p>Phone: {generalInfo.phoneNumber}</p>
 
-			
+			<h2>Education</h2>
+            <p>Degree: {educationInfo.degree}</p>
+            <p>Institution: {educationInfo.institution}</p>
+            <p>Graduation Year: {educationInfo.graduationYear}</p>
 		</div>
 	);
 }
